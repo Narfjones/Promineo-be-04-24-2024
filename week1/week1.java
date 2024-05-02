aede3b6507c2a39111307ac34fa62d6c54ba5230
Narfjones <rhile3@gmail.com>
2024-05-01 22:33:17 -0400

    added week 1

---
+++ week1/week1.java
@@ -0,0 +1,24 @@
+package week1;
+
+public class week1 {
+
+    public static void main(String[] args){
+
+        int blackJackOdds = 32; // long, unsigned long
+        double betterOdds = 32.1; // float
+        boolean flag = true;
+        char keyboardButton = 'a';
+
+        String name = "Bj Hile";
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+
+        int counter = 1;
+        System.out.println(counter);
+
+    }
+
+    
+}
